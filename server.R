e768a55dccf1b34d21425c728755022895437c3c
HuichunChien <HuichunChien@users.noreply.github.com>
2014-12-22 05:54:08 +0800

    Create server.R
    server.R file for final project of shiny app

---
+++ server.R
@@ -0,0 +1,22 @@
+library(shiny)
+
+# create AirlineFares data by my own, and save in local disk.
+# load airline data, column name, and row names. 
+AirlineFares <- as.matrix(read.csv("AirlineFares.csv"))
+colnames(AirlineFares) <- c("American Airlines", "United Airlines", "US Airways", "Delta Air Lines", "Southwest Airlines", "Virgin America", "JetBlue")
+rownames(AirlineFares) <- c("Jan.", "Feb.", "Mar.", "Apr.", "May", "June", "July", "Aug.", "Sept.", "Oct.", "Nov.", "Dec.")
+
+
+# Define a server for the Shiny app
+shinyServer(function(input, output) {
+    
+    # Fill in the spot we created for a plot
+    output$phonePlot <- renderPlot({
+        
+        # Render a barplot
+        barplot(AirlineFares[,input$airlines]*1, 
+                main=input$airlines,
+                ylab="Flight Fare (dollar)",
+                xlab="Month")
+    })
+})
